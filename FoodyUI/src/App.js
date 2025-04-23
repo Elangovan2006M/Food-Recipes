2e9ea255c26264e93b819c824ec705aed85f540a
anushalakshmi0909 <anushalakshmi0909@gmail.com>
2025-04-23 12:18:59 +0530

    react first commit

---
+++ FoodyUI/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Login from './Components/Login/Login';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
