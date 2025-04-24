3ba8f28c01ae1f6f35f369a15aeb3e0a8579cb45
anushalakshmi0909 <anushalakshmi0909@gmail.com>
2025-04-24 13:45:37 +0530

    navbar commit2

--- FoodyUI/src/Components/Navbar.js
+++ FoodyUI/src/Components/Navbar.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Styles/Navbar.css';
-import {FiSearch} from 'react-icons/fi'
+import {FiSearch} from 'react-icons/fi';
+import {FaBars, FaTimes} from 'react-icons/fa';
 import PlateStream from '../Assests/Platestream.png'
 
+
+
+
 const Navbar = () => {
+const[isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+const toggleMobileMenu = () =>
+{
+  setIsMobileMenuOpen(!isMobileMenuOpen);
+}
+
   return (
     <div className="navbar-container">
       <div className="navbar">
@@ -11,7 +22,7 @@ const Navbar = () => {
           <img src={PlateStream} alt="logo" />
           <h2 className="nav-header">PlateStream</h2>
         </div>
-        <ul className="nav-content">
+        <div className={`nav-content ${isMobileMenuOpen ? 'open' : ''}`}>
           <li><a href="/">Home</a></li>
           <li><a href="/">Recipes</a></li>
           <li><a href="/">Blog</a></li>
@@ -20,7 +31,10 @@ const Navbar = () => {
           <input type='search' placeholder='Search'/>
           <FiSearch className='search-icon'/>
         </div>
-        </ul>
+        <div className='hamburger' onClick={toggleMobileMenu}>
+          {isMobileMenuOpen ? <FaTimes/> : <FaBars/>}
+        </div>
+        </div>
       </div>
     </div>
   );
