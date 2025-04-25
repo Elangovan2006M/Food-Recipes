30896349a8f6e89d2521b18e88590d8a8b5efe94
anushalakshmi0909 <anushalakshmi0909@gmail.com>
2025-04-25 11:32:20 +0530

    footer commit

---
+++ FoodyUI/src/Components/Footer.js
@@ -0,0 +1,63 @@
+import React from "react";
+import '../Styles/Footer.css';
+import PlateStream from '../Assests/Platestream.png';
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+
+const Footer = () =>
+{
+    return (
+        <div className="footer-container">
+            <div className="footer">
+                <div className="footer-about">
+                    <div className="footer-about-logo">
+                        <img src={PlateStream} alt="logo-footer"/>
+                        <h1>Plate Stream</h1>
+                    </div>
+                    <p>Watch. Cook. Share. Your favorite recipes—streamed fresh every day.</p>
+                    <div className="socialmedia">
+                        <FaFacebook className="footer-socialmedia" size={24} color="#4267B2" />
+                        <FaTwitter className="footer-socialmedia" size={24} color="#1DA1F2" />
+                        <FaInstagram className="footer-socialmedia" size={24} color="#E1306C" />
+                    </div>
+                </div>
+                <div className="quick-links">
+                    <h3>Quick Links</h3>
+                    <ul>
+                    <li><a href="\">Home</a></li>
+                    <li><a href="\recipes">Recipes</a></li>
+                    <li><a href="\blog">Blog</a></li>
+                    <li><a href="\aboutus">About Us</a></li>
+                    <li><a href="\contact">Contact</a></li>
+                    </ul>
+                </div>
+                <div className="categories">
+                    <h3>Popular Categories</h3>
+                    <ul>
+                    <li><a href="\">Indian Recipes</a></li>
+                    <li><a href="\">Breakfast Ideas</a></li>
+                    <li><a href="\">Bachelor Recipes</a></li>
+                    <li><a href="\">5 minute Meals</a></li>
+                    <li><a href="\">Cake Recipes</a></li>
+                    </ul>
+                </div>
+
+                <div className="connect">
+                    <h3>Stay in the Flavor Loop</h3>
+                    <p>Get weekly recipes, video drops, and cooking tips straight to your inbox</p>
+
+                    <div className="footer-contact">
+                        <input type="email" placeholder="Enter email to connect"/>
+                        <button type="submit">Subscribe</button>
+                    </div>
+                </div>
+
+            </div>
+            <div className="footer-ending">
+                <hr/>
+                <p>&copy; 2025 Platestream. All rights reserved.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Footer
--- FoodyUI/src/Components/Navbar.js
+++ FoodyUI/src/Components/Navbar.js
@@ -2,25 +2,32 @@ import React, { useState } from 'react';
 import '../Styles/Navbar.css';
 import {FiSearch} from 'react-icons/fi';
 import {FaBars, FaTimes} from 'react-icons/fa';
-import PlateStream from '../Assests/Platestream.png'
+import PlateStream from '../Assests/Platestream.png';
+import {useNavigate} from 'react-router-dom';
 
 
 
 
 const Navbar = () => {
 const[isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+const navigate = useNavigate();
 
 const toggleMobileMenu = () =>
 {
   setIsMobileMenuOpen(!isMobileMenuOpen);
 }
 
+const handleNavigate = () =>
+{
+  navigate('/');
+};
+
   return (
     <div className="navbar-container">
       <div className="navbar">
-        <div className="nav-brand">
-          <img src={PlateStream} alt="logo" />
-          <h2 className="nav-header">PlateStream</h2>
+        <div className="nav-brand" onClick={handleNavigate}>
+            <img src={PlateStream} alt="logo" />
+            <h2 className="nav-header">PlateStream</h2>
         </div>
         <div className={`nav-content ${isMobileMenuOpen ? 'open' : ''}`}>
           <li><a href="/">Home</a></li>
@@ -28,7 +35,7 @@ const toggleMobileMenu = () =>
           <li><a href="/">Blog</a></li>
           <li><a href="/">About Us</a></li>
         <div className='nav-search'>
-          <input type='search' placeholder='Search'/>
+          {/* <input type='text' placeholder='Search'/> */}
           <FiSearch className='search-icon'/>
         </div>
         <div className='hamburger' onClick={toggleMobileMenu}>
